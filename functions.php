352162b83905cb81c8b06e47e5368060fe7e7582
Srinath K <k_srinath@ec.iitr.ac.in>
2023-06-01 01:22:00 +0530

    assets

--- functions.php
+++ functions.php
@@ -13,4 +13,14 @@
  * @subpackage Aquila
  * @since Aquila 1.0
  */
-?>
+
+function aquila_enqueue_scripts()
+{
+    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', ['jquery'], filemtime(get_template_directory() . '/assets/main.js'), true);
+    // Enqueqing styles and scripts
+    wp_enqueue_style('style-css');
+    wp_enqueue_script('main-js');
+}
+
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
